ab918f50adec3a8663b73dbb9a72156dafa323fc
Den <denis.r2211@gmail.com>
2022-05-04 17:17:32 +0300

    first commit

---
+++ Interpolation.js
@@ -0,0 +1,9 @@
+"use strict";
+//Интерполяция (ES6)
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);
---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+incr++;   //инкремент  ++incr - постфиксная
+decr--;   //декремент  --decr - префиксная
+
+console.log(incr);
+console.log(decr);
+
+console.log(5 % 2);
+
+console.log(2 * 4 === 8);
+
+// && - " И "
+// || - " ИЛИ "
+
+const isChecked = true,
+        isClose = false;
+    
+console.log(isChecked && !isClose);
