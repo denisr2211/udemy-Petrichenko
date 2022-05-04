4b426219f207d22981ef31f3561eead666e3d3b4
Den <denis.r2211@gmail.com>
2022-05-04 19:58:10 +0300

    prob

---
+++ #git.js
@@ -0,0 +1,26 @@
+// ЛОКАЛЬНО
+
+// git init (установка репозитория)
+// git config --global user.name "Denis"
+// git config --global user.email denis.r2211@gmail.com (появляется файл "config")
+
+// git status (статус файлов)
+// git add -A (добавить все файлы и изменения в индекс репозитория)
+// git status (проверка статуса)
+// git commit -a -m"first commit" (создание контрольной точки)
+// git log (проверить какие комиты были и их авторы)
+
+
+// Git Hub
+
+// echo "# gergr" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin git@github.com:denisr2211/gergr.git
+// git push -u origin main
+
+// git remote add origin git@github.com:denisr2211/gergr.git
+// git branch -M main
+// git push -u origin main
